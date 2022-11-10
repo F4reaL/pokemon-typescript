31f4a223b44a5c9ba773a1709d48a7803c272b1a
F4reaL <superjuniorandren11@gmail.com>
2022-11-10 17:54:53 +0700

    update

--- src/components/PokemonCollection.tsx
+++ src/components/PokemonCollection.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
-import PokemonDetail from "./PokemonDetail";
+import PokemonDetails from "./PokemonDetail";
 
 interface Pokemons {
   name: string;
@@ -16,19 +16,39 @@ interface Pokemon {
     front_default: string;
   };
 }
+export interface PokemonDetail extends Pokemon {
+  abilities?: {
+    ability:string;
+    name:string;
+  }[];
+  stats?:{
+    name:string;
+  }[]
+}
 
-const LIMIT:number = 20
+const LIMIT: number = 20;
+
 const PokemonCollection: React.FC = () => {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-  const [nextUrl, setNextUrl] = useState<string>('')
-  const [currentPokemon, setCurrentPokemon] = useState()
+  const [nextUrl, setNextUrl] = useState<string>("");
+  const [currentPokemon, setCurrentPokemon] = useState<PokemonDetail>({
+    id:0,
+    name:"",
+    sprites: {
+      front_default: ''
+    },
+  });
+
+  //Modal state
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
-  const handleShow = async(pokeName: string) => {
-    const curPoke = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
-    setCurrentPokemon(curPoke.data)
-    setShow(true)
+  const handleShow =  (id: number) => {
+     pokemons.forEach(p =>{
+      if(p.id === id)
+      setCurrentPokemon(p);
 
+    })
+    setShow(true);
   };
 
   useEffect(() => {
@@ -36,35 +56,37 @@ const PokemonCollection: React.FC = () => {
       const res = await axios.get(
         `https://pokeapi.co/api/v2/pokemon?limit=${LIMIT}&offset=20`
       );
-      setNextUrl(res.data.next)
+      setNextUrl(res.data.next);
       res.data.results.forEach(async (pokemon: Pokemons) => {
         const poke = await axios.get(
           `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
         );
-      console.log(poke.data)
-
         setPokemons((p) => [...p, poke.data]);
       });
     };
     getPokemon();
   }, []);
 
-  const handleLoadmore = async ()=>{
-    const res = await axios.get(nextUrl)
-    setNextUrl(res.data.next)
+  const handleLoadmore = async () => {
+    const res = await axios.get(nextUrl);
+    setNextUrl(res.data.next);
     res.data.results.forEach(async (pokemon: Pokemons) => {
       const poke = await axios.get(
         `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
       );
       setPokemons((p) => [...p, poke.data]);
     });
-  }
+  };
 
   return (
     <div className="pokemon-collection">
       {pokemons.map((pokemon) => {
         return (
-          <div className="pokemon" key={pokemon.id} onClick={()=> handleShow(pokemon.name)}>
+          <div
+            className="pokemon"
+            key={pokemon.id}
+            onClick={() => handleShow(pokemon.id)}
+          >
             <img
               src={pokemon.sprites.front_default}
               alt=""
@@ -74,15 +96,16 @@ const PokemonCollection: React.FC = () => {
           </div>
         );
       })}
+
       <div className="btn-loadmore" onClick={handleLoadmore}>
         <button>Load more</button>
       </div>
       <Modal show={show} onHide={handleClose}>
-        
         <Modal.Body>
-          <PokemonDetail />
+          <PokemonDetails
+            currentPokemon={currentPokemon}
+          />
         </Modal.Body>
-        
       </Modal>
     </div>
   );
--- src/components/PokemonDetail.tsx
+++ src/components/PokemonDetail.tsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React from "react";
+import { PokemonDetail } from "./PokemonCollection";
 
-interface Props{
-  
+interface Props {
+  currentPokemon: PokemonDetail;
+
 }
-const PokemonDetail:React.FC = (props) => {
+const PokemonDetails: React.FC<Props> = (props) => {
+  const { currentPokemon } = props;
+  console.log(currentPokemon.abilities?.map(ab => ab.name)); 
   return (
     <div>
-
+      <div className="pokemon-name">{currentPokemon.name}</div>
+      <img
+        src={currentPokemon.sprites.front_default}
+        alt=""
+        className="pokemon-img"
+      />
+      <div className="details">
+        <div className="detail">
+          {
+            // currentPokemon.abilities.map(ab => ab.ability)
+          }
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PokemonDetail
+export default PokemonDetails;
