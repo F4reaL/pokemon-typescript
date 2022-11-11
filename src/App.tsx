28db8440e11e2edccf21e6f1b380956a8e0a5d94
F4reaL <superjuniorandren11@gmail.com>
2022-11-12 00:45:44 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Home from './pages/Home'
-const  App:React.FC = () =>{
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Home from "./pages/Home";
+import NavbarComponent from "./components/NavbarComponent";
+
+const App: React.FC = () => {
   return (
     <div className="App">
+      <NavbarComponent />
       <h2>Pokemon'</h2>
       <Home />
-      
     </div>
   );
-}
+};
 
 export default App;
